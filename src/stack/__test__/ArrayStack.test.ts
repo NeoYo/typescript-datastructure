2255d9b5f59888c668288cf61055c5f53a083e6b
Yo <liveabean@163.com>
2018-08-20 00:45:02 +0800

    refactor: ArrayStack and LinkedListStack

--- src/stack/__test__/ArrayStack.test.ts
+++ src/stack/__test__/ArrayStack.test.ts
@@ -1,13 +1,21 @@
 import test from 'ava';
-import { StackType } from '../Stack';
-import { stackFromNumbers } from './Stack.test';
+import { StackType, StackFactory } from '../Stack';
 
-test('ArrayStack should push data', t => {
+function stackFromNumbers(stackType:StackType) {
+	const stack = new StackFactory().getStack(stackType);
+
+	stack.push(1);
+	stack.push(2);
+	stack.push(3);
+	return stack;
+}
+
+test('ArrayStack should push number', t => {
 	const stack = stackFromNumbers(StackType.ArrayStack);
 	t.is(stack.toString(), '1,2,3');
 });
 
-test('ArrayStack should pop', t => {
+test('ArrayStack should pop number', t => {
 	const stack = stackFromNumbers(StackType.ArrayStack);
 	t.is(stack.pop(), 3);
 	t.is(stack.pop(), 2);
